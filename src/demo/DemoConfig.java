b8885b2740c4a9a3b4298cc0f645f128b7698b3e
zhaojunjun <zhaojunjun0531@126.com>
2016-07-04 22:32:30 +0800

    git demo commit

---
+++ src/demo/DemoConfig.java
@@ -0,0 +1,41 @@
+package demo;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+
+public class DemoConfig extends JFinalConfig{
+
+	@Override
+	public void configConstant(Constants arg0) {
+		arg0.setDevMode(true);
+	}
+
+	@Override
+	public void configHandler(Handlers arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configInterceptor(Interceptors arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configPlugin(Plugins arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configRoute(Routes arg0) {
+		arg0.add("/hello", HelloController.class);
+	}
+	
+
+}
